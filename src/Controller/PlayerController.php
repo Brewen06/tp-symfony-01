9176b09a6120316aa8a1d19871a7e45f4cc4a41f
frotin <frotin@sio.ndlp>
2025-09-05 14:55:51 +0200

    Modifications de données

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -11,10 +11,33 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PlayerController extends AbstractController
 {
-    #[Route('/player/', name:'app_player')]
-    public function index(PlayerRepository $playerRepository): Response
+    private PlayerRepository $playerRepository;
+    private EntityManagerInterface $entityManager;
+
+
+    public function __construct(PlayerRepository $playerRepository, EntityManagerInterface $entityManager)
     {
-        $players = $playerRepository->findAll();
+        $this->playerRepository = $playerRepository;
+        $this->entityManager = $entityManager;
+    }
+    
+    #[Route('player/delete/{id}', name: 'delete_player')]
+    public function delete(int $id): Response
+    {
+        $player = $this->playerRepository->find($id);
+        if ($player) {
+            $this->entityManager->remove($player);
+            $this->entityManager->flush();
+            return new Response('Player with id' .$id. 'deleted');
+        } else {
+            return new Response('Player with id' .$id. 'not found', 404);
+        }
+    }
+
+    #[Route('/player/', name:'app_player')]
+    public function index(): Response
+    {
+        $players = $this->playerRepository->findAll();
 
         return $this->render('player/index.html.twig', [
             'players' => $players,
@@ -22,14 +45,16 @@ class PlayerController extends AbstractController
     }
 
     #[Route('/player/create', name: 'create_player')]
-    public function create(EntityManagerInterface $entityManager): Response
+    public function create(): Response
     {
         $player = new Player();
-        $player->setName('Mario');
+        $player->setName('Link');
+        $player->setXp('100');
 
-        $entityManager->persist($player);
+        $this->$entityManager->persist($player);
         $entityManager->flush();
 
         return new Response('Player created with id '.$player->getId());
     }
+
 }
